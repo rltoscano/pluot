5084433b74dc23fdd8bda3e3e210ff9e25778df9
Robert Toscano <github@gombi.net>
2017-09-12 20:17:54 -0700

    Refactor due to pihen API change.

--- service/src/github.com/rltoscano/pluot/service.go
+++ service/src/github.com/rltoscano/pluot/service.go
@@ -7,53 +7,49 @@ import (
 )
 
 func init() {
-	collections := []pihen.Collection{
-		{
-			URL: "/svc/txns",
-			Methods: map[string]pihen.Method{
-				http.MethodGet:   listTxns,
-				http.MethodPatch: patchTxns,
+	api := pihen.API{
+		Collections: []pihen.Collection{
+			{
+				URL: "/svc/txns",
+				Methods: map[string]pihen.Method{
+					http.MethodGet:   listTxns,
+					http.MethodPatch: patchTxns,
+				},
 			},
-			AllowedOrigin: "http://localhost:8081",
-		},
-		{
-			URL:           "/svc/txns/",
-			Methods:       map[string]pihen.Method{http.MethodPatch: patchTxn},
-			AllowedOrigin: "http://localhost:8081",
-		},
-		{
-			URL:           "/svc/uploads",
-			Methods:       map[string]pihen.Method{http.MethodPost: createUpload},
-			AllowedOrigin: "http://localhost:8081",
-		},
-		{
-			URL:           "/svc/uploads:check",
-			Methods:       map[string]pihen.Method{http.MethodPost: checkUpload},
-			AllowedOrigin: "http://localhost:8081",
-		},
-		{
-			URL: "/svc/rules",
-			Methods: map[string]pihen.Method{
-				http.MethodGet:  listRules,
-				http.MethodPost: createRule,
+			{
+				URL:     "/svc/txns/",
+				Methods: map[string]pihen.Method{http.MethodPatch: patchTxn},
 			},
-			AllowedOrigin: "http://localhost:8081",
+			{
+				URL:     "/svc/uploads",
+				Methods: map[string]pihen.Method{http.MethodPost: createUpload},
+			},
+			{
+				URL:     "/svc/uploads:check",
+				Methods: map[string]pihen.Method{http.MethodPost: checkUpload},
+			},
+			{
+				URL: "/svc/rules",
+				Methods: map[string]pihen.Method{
+					http.MethodGet:  listRules,
+					http.MethodPost: createRule,
+				},
+			},
+			{
+				URL:     "/svc/rules:proposal",
+				Methods: map[string]pihen.Method{http.MethodGet: listRuleProposals},
+			},
+			{
+				URL:     "/svc/rules/",
+				Methods: map[string]pihen.Method{http.MethodDelete: deleteRule},
+			},
+			{
+				URL:     "/svc/aggs",
+				Methods: map[string]pihen.Method{http.MethodPost: computeAggregation},
+			},
 		},
-		{
-			URL:           "/svc/rules:proposal",
-			Methods:       map[string]pihen.Method{http.MethodGet: listRuleProposals},
-			AllowedOrigin: "http://localhost:8081",
-		},
-		{
-			URL:           "/svc/rules/",
-			Methods:       map[string]pihen.Method{http.MethodDelete: deleteRule},
-			AllowedOrigin: "http://localhost:8081",
-		},
-		{
-			URL:           "/svc/aggs",
-			Methods:       map[string]pihen.Method{http.MethodPost: computeAggregation},
-			AllowedOrigin: "http://localhost:8081",
-		},
+		AllowedOrigin:      "http://localhost:8081",
+		UserEmailWhitelist: []string{"test@example.com"},
 	}
-	pihen.Bind(collections)
+	pihen.Bind(api)
 }
