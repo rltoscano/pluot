d06fa0cae2cb7813ce0fa8170ba6a83a921dc5e2
Robert Toscano <github@gombi.net>
2017-09-17 16:58:06 -0700

    Add tool for packaging webapp and copying to serving folder.

---
+++ tool/package_webapp.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	println("Building polymer webapp.")
+	c := exec.Command("polymer", "build")
+	c.Dir = "../webapp"
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	err := c.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v", err)
+		return
+	}
+
+	println("Removing old build.")
+	err = os.RemoveAll("../service/src/github.com/rltoscano/pluot/web")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v", err)
+		return
+	}
+
+	println("Copying new build.")
+	c = exec.Command("powershell", "copy", "../webapp/build/default", "../service/src/github.com/rltoscano/pluot/web", "-recurse")
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	err = c.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v", err)
+		return
+	}
+}
