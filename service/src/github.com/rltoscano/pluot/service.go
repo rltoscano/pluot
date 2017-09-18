b329967b30b7169444445729d7be6dd3e8cd63d9
Robert Toscano <github@gombi.net>
2017-09-17 18:08:51 -0700

    Add authorized user checks.

--- service/src/github.com/rltoscano/pluot/service.go
+++ service/src/github.com/rltoscano/pluot/service.go
@@ -1,9 +1,14 @@
 package pluot
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/rltoscano/pihen"
+	"golang.org/x/net/context"
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/user"
 )
 
 func init() {
@@ -48,8 +53,36 @@ func init() {
 				Methods: map[string]pihen.Method{http.MethodPost: computeAggregation},
 			},
 		},
-		AllowedOrigin:      "http://localhost:8081",
-		UserEmailWhitelist: []string{"test@example.com"},
+		AllowedOrigin: "http://localhost:8081",
+		Interceptor:   authorizeUser,
 	}
 	pihen.Bind(api)
+}
+
+type config struct {
+	// List of comma-separated emails of authorized users.
+	AuthorizedUsers string `datastore:"AuthorizedUsers,noindex"`
+}
+
+func authorizeUser(c context.Context, r *http.Request, u *user.User) error {
+	if u == nil {
+		return pihen.Error{http.StatusUnauthorized, "A user could not be determined from the request."}
+	}
+	var conf config
+	k := datastore.NewKey(c, "Config", "singleton", 0, nil)
+	err := datastore.Get(c, k, &conf)
+	if err == datastore.ErrNoSuchEntity {
+		// Create empty config.
+		_, err = datastore.Put(c, k, &conf)
+	}
+	if err != nil {
+		return err
+	}
+	emails := strings.Split(conf.AuthorizedUsers, ",")
+	for _, authorized := range emails {
+		if authorized == u.Email {
+			return nil
+		}
+	}
+	return pihen.Error{http.StatusUnauthorized, fmt.Sprintf("The user %s is unauthorized.", u.Email)}
 }
