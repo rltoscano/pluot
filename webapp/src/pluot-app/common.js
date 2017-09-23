834a2b39750a5afd086e22d5e9b5de144c828644
Robert Toscano <github@gombi.net>
2017-09-23 16:53:26 -0700

    Remove unused function.

--- webapp/src/pluot-app/common.js
+++ webapp/src/pluot-app/common.js
@@ -3,17 +3,6 @@ function formatAmount(amount) {
   return "$" + amountStr.substr(0, amountStr.length-2) + "." + amountStr.substr(-2);
 }
 
-function formatCategory(category) {
-  switch (category) {
-    case 1: return 'Uncategorized';
-    case 2: return 'Entertainment';
-    case 3: return 'Eating Out';
-    case 4: return 'Groceries';
-    case 5: return 'Shopping';
-    case 6: return 'Health';
-  }
-}
-
 function formatDate(str) {
   var d = new Date(Date.parse(str));
   return d.toLocaleString('en-US', { month: 'short', day: 'numeric' });
