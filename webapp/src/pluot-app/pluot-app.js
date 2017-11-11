858a85c928d705251296652d37ffc3c06bb085d9
Robert Toscano <github@gombi.net>
2017-11-11 15:55:06 -0800

    [webapp] Split pluot-app into separate files.

---
+++ webapp/src/pluot-app/pluot-app.js
@@ -0,0 +1,56 @@
+/**
+ * @customElement
+ * @polymer
+ */
+class PluotApp extends Polymer.Element {
+  static get is() { return 'pluot-app'; }
+  static get properties() {
+    return {
+      txnsSearching: { type: Boolean, value: false, observer: '_clearTxnSearch' },
+      subroute: { type: Object, observer: '_updateTxnSearch' },
+      queryStr: { type: String, observer: '_updateSubrouteToQueryStr' },
+    };
+  }
+  navToGitHubIssues() {
+    window.location = "https://github.com/rltoscano/pluot/issues/new";
+  }
+  ready() {
+    super.ready();
+    if (!this.route.path) {
+      this.set('route.path', "/agg");
+    }
+  }
+  _setTxnsModeToAdd(evt) { this.$.txnsList.mode = 'add'; }
+  _setTxnsModeToList(evt) { this.$.txnsList.mode = 'list'; }
+  _txnsModeNotAdd(txnsMode) { return txnsMode != 'add'; }
+  _txnsModeNotList(txnsMode) { return txnsMode != 'list'; }
+  _txnsModeNotListOrNotSearching(txnsMode, txnsSearching) {
+    return txnsMode != 'list' || !txnsSearching;
+  }
+  _txnsModeIsList(txnsMode) { return txnsMode == 'list'; }
+  _updateTxnSearch(subroute) {
+    if (subroute.path && subroute.path.substr(1)) {
+      this.txnsSearching = true;
+      this.$.txnSearch.queryStr = subroute.path.substr(1);
+    } else {
+      this.txnsSearching = false;
+    }
+  }
+  _updateSubrouteToQueryStr(queryStr) {
+    var subroutePath;
+    if (queryStr) {
+      subroutePath = '/' + queryStr;
+    } else {
+      subroutePath = '';
+    }
+    this.set('subroute.path', subroutePath);
+  }
+  _closeDrawer() { this.$.drawer.opened = false; }
+  _clearTxnSearch() { this.$.txnSearch.queryStr = ''; }
+  _rulesEditNew() { this.$.rules.editNew(); }
+  _rulesDelete() { this.$.rules.delete(); }
+  _rulesConfirmEdit() { this.$.rules.confirmEdit(); }
+
+}
+
+window.customElements.define(PluotApp.is, PluotApp);
